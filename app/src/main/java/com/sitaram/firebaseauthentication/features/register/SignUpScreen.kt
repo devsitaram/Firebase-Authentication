2f1b1957433d40fe34fd4ec102e5905178267b72
Sita Ram Thing <sitaramthing667@gmail.com>
2023-07-17 13:33:56 +0545

    Design for login and register screen

--- app/src/main/java/com/sitaram/firebaseauthentication/features/register/SignUpScreen.kt
+++ app/src/main/java/com/sitaram/firebaseauthentication/features/register/SignUpScreen.kt
@@ -1,8 +1,89 @@
 package com.sitaram.firebaseauthentication.features.register
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sitaram.firebaseauthentication.R
+import com.sitaram.firebaseauthentication.features.util.ButtonComponent
+import com.sitaram.firebaseauthentication.features.util.NavigationTextComponent
+import com.sitaram.firebaseauthentication.features.util.TextComponent
+import com.sitaram.firebaseauthentication.features.util.TextFieldComponent
 
+@Preview
 @Composable
 fun SignUpViewScreen() {
 
+    var username by remember {
+        mutableStateOf("")
+    }
+
+    var password by remember {
+        mutableStateOf("")
+    }
+
+    val onLoginClick: () -> Unit = {
+
+    }
+
+    Surface(Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier.padding(25.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+
+            Spacer(modifier = Modifier.height(50.dp))
+            TextComponent(text = "Register your details", color = Color.DarkGray)
+            Spacer(modifier = Modifier.height(50.dp))
+
+            Column {
+                TextFieldComponent(
+                    password,
+                    onValueChange = { password = it },
+                    painter = painterResource(id = R.drawable.ic_email),
+                    text = "Email",
+                )
+
+                Spacer(modifier = Modifier.height(10.dp))
+                TextFieldComponent(
+                    username,
+                    onValueChange = { username = it },
+                    painter = painterResource(id = R.drawable.ic_person),
+                    text = "Username",
+                )
+
+                Spacer(modifier = Modifier.height(10.dp))
+                TextFieldComponent(
+                    password,
+                    onValueChange = { password = it },
+                    painter = painterResource(id = R.drawable.ic_lock),
+                    text = "Password",
+                )
+
+                Spacer(modifier = Modifier.height(50.dp))
+                // normal button
+                ButtonComponent(onClick = onLoginClick, btnText = "Sign Up")
+            }
+            // divider
+            Divider(Modifier.padding(top = 30.dp, start = 25.dp, end = 25.dp).fillMaxWidth().height(3.dp).background(Color.White))
+
+            NavigationTextComponent(text = "Login your account?", color = Color.DarkGray)
+        }
+    }
 }
---
+++ app/src/main/java/com/sitaram/firebaseauthentication/features/util/ui_util.kt
@@ -0,0 +1,107 @@
+package com.sitaram.firebaseauthentication.features.util
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun TextComponent(text: String, color: Color) {
+    Text(
+        text = text,
+        modifier = Modifier
+            .wrapContentHeight()
+            .padding(top = 5.dp),  // Specify the desired padding value
+        style = TextStyle(
+            fontSize = 26.sp,
+            fontWeight = FontWeight.SemiBold,
+            fontStyle = FontStyle.Normal
+        ),
+        textAlign = TextAlign.Center,
+        color = color
+    )
+}
+
+@Composable
+fun NavigationTextComponent(text: String, color: Color) {
+    Text(
+        text = text,
+        modifier = Modifier
+            .wrapContentHeight()
+            .padding(top = 5.dp),  // Specify the desired padding value
+        style = TextStyle(
+            fontSize = 20.sp,
+            fontWeight = FontWeight.SemiBold,
+            fontStyle = FontStyle.Normal
+        ),
+        textAlign = TextAlign.Center,
+        color = color
+    )
+}
+
+
+@Composable
+fun TextFieldComponent(
+    value: String,
+    onValueChange: (String) -> Unit = {},
+    painter: Painter,
+    text: String
+) {
+    if (value.isEmpty()) {
+        Text(
+            text = "Enter the valid $text",
+            color = Color.Red,
+            modifier = Modifier.padding(start = 5.dp)
+        )
+    }
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        placeholder = { Text(text = text) },
+        leadingIcon = {
+            Icon(
+                painter = painter,
+                contentDescription = null
+            ) // painterResource(id = R.drawable.ic_person)
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(5.dp)
+            .background(
+                color = Color.White
+            ),
+    )
+}
+
+@Composable
+fun ButtonComponent(onClick: () -> Unit = {}, btnText: String) {
+    // normal button
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp),
+        colors = ButtonDefaults.buttonColors(backgroundColor = Color.Red) // Change the button color here
+    ) {
+        androidx.compose.material.Text(
+            fontSize = 20.sp,
+            text = btnText,
+            fontWeight = FontWeight.SemiBold,
+        )
+    }
+}
