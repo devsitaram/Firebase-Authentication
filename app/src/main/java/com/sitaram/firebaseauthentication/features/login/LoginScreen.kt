4fba0e6b2b101d78109d5849e82cf8229717a587
Sita Ram Thing <sitaramthing667@gmail.com>
2023-07-17 12:06:53 +0545

    add the new dependency for firebase authentication

--- app/src/main/java/com/sitaram/firebaseauthentication/features/login/LoginScreen.kt
+++ app/src/main/java/com/sitaram/firebaseauthentication/features/login/LoginScreen.kt
@@ -2,18 +2,25 @@ package com.sitaram.firebaseauthentication.features.login
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sitaram.firebaseauthentication.features.TextComponent
 
 @Preview
 @Composable
 fun LoginViewScreen() {
     Surface(Modifier.fillMaxSize()) {
-        Column {
-            Text(text = "Login your details")
+        Column(
+            modifier = Modifier.padding(10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,) {
+            TextComponent(text = "Login your details", color = Color.DarkGray)
         }
     }
 }
